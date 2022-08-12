b541422ea2228bcecd8dd55216a1f9c1c2a89e99
Dalyn Dalton <dalyn.dalton.dd@gmail.com>
2022-08-12 16:43:33 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import SideBarVue from "./components/SideBar.vue";
+</script>
+
+<template>
+  <div id="body" class="flex">
+    <div id="nav" class="inline-flex lg:flex-col flex-row w-screen lg:h-screen lg:w-auto content-center bg-gray-200">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+a {
+  @apply font-normal hover:font-bold rounded-lg p-1;
+}
+
+a.router-link-exact-active {
+  @apply bg-yellow-200;
+}
+</style>
